59118b3d1dfa7c8572faeb4db633275da2d460fd
Ashmitha <ashmitha.1402@gmail.com>
2026-04-08 14:21:07 +0530

    dev-2: updated EmailService

---
+++ backend/src/main/java/com/tms/backend/service/EmailService.java
@@ -0,0 +1,29 @@
+package com.tms.backend.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendEmail(String toEmail, String subject, String body) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom("noreply@tms.com"); 
+            message.setTo(toEmail);
+            message.setSubject(subject);
+            message.setText(body);
+
+            mailSender.send(message);
+            System.out.println("Mail Sent Successfully to " + toEmail);
+            
+        } catch (Exception e) {
+            System.err.println("Failed to send email: " + e.getMessage());
+        }
+    }
+}
